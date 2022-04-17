1acb739ad16b67206aa369413fc7d477a9e4a466
johnnyreilly <johnny_reilly@hotmail.com>
2022-04-17 18:04:00 +0000

    send email

--- WeatherService/Controllers/WeatherForecastController.cs
+++ WeatherService/Controllers/WeatherForecastController.cs
@@ -17,9 +17,7 @@ public class WeatherForecastController : ControllerBase
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-#pragma warning disable IDE0052
     private readonly ILogger<WeatherForecastController> _logger;
-#pragma warning restore IDE0052
     private readonly MailConfig _options;
 
     public WeatherForecastController(
@@ -30,22 +28,46 @@ public class WeatherForecastController : ControllerBase
         _logger = logger;
         _options = options.Value;
     }
+    
+    public record SendWeatherForecastBody(string Email);
 
-    [HttpGet(Name = "GetWeatherForecast")]
-    public async Task<IEnumerable<WeatherForecast>> Get()
+    [HttpPost(Name = "SendWeatherForecast")]
+    public async Task<string> Post(SendWeatherForecastBody body)
     {
-        await SendSimpleMessage();
-
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        try
         {
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = _options.Name + _options.Title + Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+            if (string.IsNullOrEmpty(body.Email)) throw new Exception("Email required");
+
+            var weatherForecast = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = _options.Name + _options.Title + Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+
+            var toEmailAddress = body.Email;
+            var text = $@"The weather forecast is:
+
+{string.Join("\n", weatherForecast.Select(wf => $"On {wf.Date} the weather will be {wf.Summary}"))}        
+";
+
+            await SendSimpleMessage(
+                toEmailAddress: toEmailAddress,
+                text: text
+            );
+
+            return $"We have mailed {toEmailAddress} with the following:\n\n{text}";
+        }
+        catch (Exception exc)
+        {
+            _logger.LogError(exc, $"Problem!");
+            
+            return exc.Message;
+        }
     }
 
-    async Task<RestResponse> SendSimpleMessage()
+    async Task<RestResponse> SendSimpleMessage(string toEmailAddress, string text)
     {
         RestClient client = new(new RestClientOptions
         {
@@ -58,10 +80,10 @@ public class WeatherForecastController : ControllerBase
         RestRequest request = new();
         request.AddParameter("domain", "mg.priou.co.uk", ParameterType.UrlSegment);
         request.Resource = "{domain}/messages";
-        request.AddParameter("from", "John Reilly <johnnypaulreilly@gmail.com>");
-        request.AddParameter("to", "johnny_reilly@hotmail.com");
+        request.AddParameter("from", "John Reilly <johnny_reilly@hotmail.com>");
+        request.AddParameter("to", toEmailAddress);
         request.AddParameter("subject", "Hello");
-        request.AddParameter("text", "Testing some Mailgun awesomeness!");
+        request.AddParameter("text", text);
 
         return await client.PostAsync(request);
     }
--- WebService/index.ts
+++ WebService/index.ts
@@ -1,4 +1,6 @@
 import Koa from 'koa';
+import Router from '@koa/router';
+import koaBody from 'koa-body';
 import axios from 'axios';
 
 // How we connect to the dotnet service with dapr
@@ -9,27 +11,62 @@ const weatherServiceAppIdHeaders = {
 };
 
 const app = new Koa();
+const router = new Router();
 
-app.use(async ctx => {
+app.use(async (ctx, next) => {
     try {
-        const data = await axios.get<WeatherForecast[]>(`${daprSidecarBaseUrl}/weatherForecast`, {
-            headers: weatherServiceAppIdHeaders
-        });
+        await next()
+        const status = ctx.status || 404;
+        if (status === 404) ctx.throw(404);
+    } catch (err: any) {
+        ctx.status = err.status || 500;
+        ctx.body = (ctx.status === 404) ? 'not found alas' : `hmmm: ${ctx.status}`;
+    }
+})
 
-        ctx.body = `And the weather today will be ${data.data[0].summary}`;
+const formHtml = (header: string) => `<!DOCTYPE html>
+<html>
+<head>
+<title>Email me!</title>
+</head>
+<body>
+<form method="post">
+    <h1>${header}</h1>
+    <label for="email">Enter your email:</label>
+    <input type="email" id="email" name="email" required>
+    <button type="submit">Submit</button>
+</form>
+</body>
+</html>
+`;
+
+router.get('/', async (ctx, next) => {
+    ctx.body = formHtml("We'd like to email you")
+});
+
+router.post('/', koaBody(), async (ctx, next) => {
+    try {
+        if (ctx.request.body.email) {
+            await axios.post(`${daprSidecarBaseUrl}/weatherForecast`, {
+                email: ctx.request.body.email
+            }, {
+                headers: weatherServiceAppIdHeaders
+            });
+
+            ctx.body = formHtml("Message sent");
+        } else {
+            ctx.body = formHtml("No email supplied");
+        }
     } catch (exc) {
         console.error('Problem calling weather service', exc)
         ctx.body = 'Something went wrong!'
     }
 });
 
+app
+    .use(router.routes())
+    .use(router.allowedMethods());
+
 const portNumber = 3000;
 app.listen(portNumber);
 console.log(`listening on port ${portNumber}`);
-
-interface WeatherForecast {
-    date: string;
-    temperatureC: number;
-    temperatureF: number;
-    summary: string;
-}
